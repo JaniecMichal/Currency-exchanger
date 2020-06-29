afe5891144d53995d53a67f1639c0324f6da1826
Michal Janiec <michal.janiec95@gmail.com>
2020-06-30 00:06:43 +0200

    Completly Function Refactored

--- js/CurrencyExchanger.js
+++ js/CurrencyExchanger.js
@@ -1,120 +1,162 @@
+{
+    const FromCurrencySymbol = document.querySelector(".js-FromCurrencySymbol");
 
-let form = document.querySelector(".form");
-let FromCurrency = document.querySelectorAll(".js-FromCurrency");
-let ToCurrency = document.querySelectorAll(".js-ToCurrency");
-let FromValue = document.querySelector(".js-FromValue");
-let FromCurrencySymbol = document.querySelector(".js-FromCurrencySymbol");
-let Rate = document.querySelector(".js-Rate");
-let RateCurrencySymbol = document.querySelector(".js-RateCurrencySymbol");
-let ToCurrencySymbol = document.querySelector(".js-ToCurrencySymbol");
-let ToValue = document.querySelector(".js-ToValue")
-let EuroRate = 4.4573;
-let GBPRate = 4.9328;
-let USDRate = 3.9741;
-let CHFRate = 4.1811;
-let PLNRate = 1.0;
-
-const CheckFromCurrency = () => {
-    for (i = 1; i <= 5; i++) {
-        if (FromCurrency[i - 1].checked === true) {
-            FromCurrencySymbol.value = FromCurrency[i - 1].value;
-            break;
+    const CheckFromCurrency = () => {
+        const FromCurrency = document.querySelectorAll(".js-FromCurrency");
+        for (i = 1; i <= 5; i++) {
+            if (FromCurrency[i - 1].checked === true) {
+                FromCurrencySymbol.value = FromCurrency[i - 1].value;
+                break;
+            }
         }
-    }
-}
-form.addEventListener("input", CheckFromCurrency);
+    };
 
 
-const CheckToCurrency = () => {
-    for (i = 1; i <= 5; i++) {
-        if (ToCurrency[i - 1].checked === true) {
-            ToCurrencySymbol.value = ToCurrency[i - 1].value;
-            break;
+    const ToCurrencySymbol = document.querySelector(".js-ToCurrencySymbol");
+
+    const CheckToCurrency = () => {
+        const ToCurrency = document.querySelectorAll(".js-ToCurrency");
+        for (i = 1; i <= 5; i++) {
+            if (ToCurrency[i - 1].checked === true) {
+                ToCurrencySymbol.value = ToCurrency[i - 1].value;
+                break;
+            }
         }
+    };
+
+
+    const Rate = document.querySelector(".js-Rate")
+
+    const SetCurrencyRate = () => {
+
+        const EuroRate = 4.4631;
+        const GBPRate = 4.8900;
+        const USDRate = 3.9656;
+        const CHFRate = 4.1893;
+        const PLNRate = 1.00;
+
+        if (FromCurrencySymbol.value === "EUR") {
+            switch (ToCurrencySymbol.value) {
+                case "EUR":
+                    Rate.value = (EuroRate / EuroRate).toFixed(2);
+                    break;
+                case "USD":
+                    Rate.value = (EuroRate / USDRate).toFixed(2);
+                    break;
+                case "GBP":
+                    Rate.value = (EuroRate / GBPRate).toFixed(2);
+                    break;
+                case "CHF":
+                    Rate.value = (EuroRate / CHFRate).toFixed(2);
+                    break;
+                case "PLN":
+                    Rate.value = (EuroRate / PLNRate).toFixed(2);
+                    break;
+            }
+        }
+
+        if (FromCurrencySymbol.value === "USD") {
+            switch (ToCurrencySymbol.value) {
+                case "EUR":
+                    Rate.value = (USDRate / EuroRate).toFixed(2);
+                    break;
+                case "USD":
+                    Rate.value = (USDRate / USDRate).toFixed(2);
+                    break;
+                case "GBP":
+                    Rate.value = (USDRate / GBPRate).toFixed(2);
+                    break;
+                case "CHF":
+                    Rate.value = (USDRate / CHFRate).toFixed(2);
+                    break;
+                case "PLN":
+                    Rate.value = (USDRate / PLNRate).toFixed(2);
+                    break;
+            }
+        }
+
+        if (FromCurrencySymbol.value === "GBP") {
+            switch (ToCurrencySymbol.value) {
+                case "EUR":
+                    Rate.value = (GBPRate / EuroRate).toFixed(2);
+                    break;
+                case "USD":
+                    Rate.value = (GBPRate / USDRate).toFixed(2);
+                    break;
+                case "GBP":
+                    Rate.value = (GBPRate / GBPRate).toFixed(2);
+                    break;
+                case "CHF":
+                    Rate.value = (GBPRate / CHFRate).toFixed(2);
+                    break;
+                case "PLN":
+                    Rate.value = (GBPRate / PLNRate).toFixed(2);
+                    break;
+            }
+        }
+
+        if (FromCurrencySymbol.value === "CHF") {
+            switch (ToCurrencySymbol.value) {
+                case "EUR":
+                    Rate.value = (CHFRate / EuroRate).toFixed(2);
+                    break;
+                case "USD":
+                    Rate.value = (CHFRate / USDRate).toFixed(2);
+                    break;
+                case "GBP":
+                    Rate.value = (CHFRate / GBPRate).toFixed(2);
+                    break;
+                case "CHF":
+                    Rate.value = (CHFRate / CHFRate).toFixed(2);
+                    break;
+                case "PLN":
+                    Rate.value = (CHFRate / PLNRate).toFixed(2);
+                    break;
+            }
+        }
+
+        if (FromCurrencySymbol.value === "PLN") {
+            switch (ToCurrencySymbol.value) {
+                case "EUR":
+                    Rate.value = (PLNRate / EuroRate).toFixed(2);
+                    break;
+                case "USD":
+                    Rate.value = (PLNRate / USDRate).toFixed(2);
+                    break;
+                case "GBP":
+                    Rate.value = (PLNRate / GBPRate).toFixed(2);
+                    break;
+                case "CHF":
+                    Rate.value = (PLNRate / CHFRate).toFixed(2);
+                    break;
+                case "PLN":
+                    Rate.value = (PLNRate / PLNRate).toFixed(2);
+                    break;
+            }
+        }
+
+    };
+
+
+    const ConvertValueOnSubmit = (event) => {
+        event.preventDefault();
+        const FromValue = document.querySelector(".js-FromValue");
+        const ToValue = document.querySelector(".js-ToValue")
+        const ValueToConvert = +FromValue.value;
+        ToValue.value = ValueToConvert * Rate.value;
+    };
+
+    const init = () => {
+        const form = document.querySelector(".form");
+
+        form.addEventListener("input", CheckFromCurrency);
+        form.addEventListener("input", CheckToCurrency);
+        form.addEventListener("input", SetCurrencyRate);
+        form.addEventListener("submit", ConvertValueOnSubmit)
     }
+
+    init();
 }
-form.addEventListener("input", CheckToCurrency);
-
-
-form.addEventListener("input", () => {
-
-
-    //we inform about currency rate 
-    if (FromCurrencySymbol.value == "EUR" && ToCurrencySymbol.value == "EUR") {
-        Rate.value = (EuroRate / EuroRate).toFixed(2);
-    } else if (FromCurrencySymbol.value == "EUR" && ToCurrencySymbol.value == "USD") {
-        Rate.value = (EuroRate / USDRate).toFixed(2);
-    } else if (FromCurrencySymbol.value == "EUR" && ToCurrencySymbol.value == "GBP") {
-        Rate.value = (EuroRate / GBPRate).toFixed(2);
-    } else if (FromCurrencySymbol.value == "EUR" && ToCurrencySymbol.value == "CHF") {
-        Rate.value = (EuroRate / CHFRate).toFixed(2);
-    } else if (FromCurrencySymbol.value == "EUR" && ToCurrencySymbol.value == "PLN") {
-        Rate.value = (EuroRate / PLNRate).toFixed(2);
-    }
-
-    if (FromCurrencySymbol.value == "USD" && ToCurrencySymbol.value == "EUR") {
-        Rate.value = (USDRate / EuroRate).toFixed(2);
-    } else if (FromCurrencySymbol.value == "USD" && ToCurrencySymbol.value == "USD") {
-        Rate.value = (USDRate / USDRate).toFixed(2);
-    } else if (FromCurrencySymbol.value == "USD" && ToCurrencySymbol.value == "GBP") {
-        Rate.value = (USDRate / GBPRate).toFixed(2);
-    } else if (FromCurrencySymbol.value == "USD" && ToCurrencySymbol.value == "CHF") {
-        Rate.value = (USDRate / CHFRate).toFixed(2);
-    } else if (FromCurrencySymbol.value == "USD" && ToCurrencySymbol.value == "PLN") {
-        Rate.value = (USDRate / PLNRate).toFixed(2);
-    }
-
-    if (FromCurrencySymbol.value == "GBP" && ToCurrencySymbol.value == "EUR") {
-        Rate.value = (GBPRate / EuroRate).toFixed(2);
-    } else if (FromCurrencySymbol.value == "GBP" && ToCurrencySymbol.value == "USD") {
-        Rate.value = (GBPRate / USDRate).toFixed(2);
-    } else if (FromCurrencySymbol.value == "GBP" && ToCurrencySymbol.value == "GBP") {
-        Rate.value = (GBPRate / GBPRate).toFixed(2);
-    } else if (FromCurrencySymbol.value == "GBP" && ToCurrencySymbol.value == "CHF") {
-        Rate.value = (GBPRate / CHFRate).toFixed(2);
-    } else if (FromCurrencySymbol.value == "GBP" && ToCurrencySymbol.value == "PLN") {
-        Rate.value = (GBPRate / PLNRate).toFixed(2);
-    }
-
-    if (FromCurrencySymbol.value == "CHF" && ToCurrencySymbol.value == "EUR") {
-        Rate.value = (CHFRate / EuroRate).toFixed(2);
-    } else if (FromCurrencySymbol.value == "CHF" && ToCurrencySymbol.value == "USD") {
-        Rate.value = (CHFRate / USDRate).toFixed(2);
-    } else if (FromCurrencySymbol.value == "CHF" && ToCurrencySymbol.value == "GBP") {
-        Rate.value = (CHFRate / GBPRate).toFixed(2);
-    } else if (FromCurrencySymbol.value == "CHF" && ToCurrencySymbol.value == "CHF") {
-        Rate.value = (CHFRate / CHFRate).toFixed(2);
-    } else if (FromCurrencySymbol.value == "CHF" && ToCurrencySymbol.value == "PLN") {
-        Rate.value = (CHFRate / PLNRate).toFixed(2);
-    }
-
-    if (FromCurrencySymbol.value == "PLN" && ToCurrencySymbol.value == "EUR") {
-        Rate.value = (PLNRate / EuroRate).toFixed(2);
-    } else if (FromCurrencySymbol.value == "PLN" && ToCurrencySymbol.value == "USD") {
-        Rate.value = (PLNRate / USDRate).toFixed(2);
-    } else if (FromCurrencySymbol.value == "PLN" && ToCurrencySymbol.value == "GBP") {
-        Rate.value = (PLNRate / GBPRate).toFixed(2);
-    } else if (FromCurrencySymbol.value == "PLN" && ToCurrencySymbol.value == "CHF") {
-        Rate.value = (PLNRate / CHFRate).toFixed(2);
-    } else if (FromCurrencySymbol.value == "PLN" && ToCurrencySymbol.value == "PLN") {
-        Rate.value = (PLNRate / PLNRate).toFixed(2);
-    }
-
-})
-
-
-
-form.addEventListener("submit", (event) => {
-    event.preventDefault();
-
-
-    let ValueToConvert = +FromValue.value;
-    ToValue.value = ValueToConvert * Rate.value;
-
-
-
-
-});
 
 
 
