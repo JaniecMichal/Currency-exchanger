a4ebcbb3a8fdae50d36705019a5b645c298ebc32
Michal Janiec <michal.janiec95@gmail.com>
2020-06-29 22:50:48 +0200

    FromWhichCurrency and ToWhichCurrency refactored

--- js/CurrencyExchanger.js
+++ js/CurrencyExchanger.js
@@ -1,58 +1,43 @@
 
 let form = document.querySelector(".form");
-let FromCurrency0 = document.querySelector(".js-FromCurrency0");
-let FromCurrency1 = document.querySelector(".js-FromCurrency1");
-let FromCurrency2 = document.querySelector(".js-FromCurrency2");
-let FromCurrency3 = document.querySelector(".js-FromCurrency3");
-let FromCurrency4 = document.querySelector(".js-FromCurrency4");
-let ToCurrency0 = document.querySelector(".js-ToCurrency0");
-let ToCurrency1 = document.querySelector(".js-ToCurrency1");
-let ToCurrency2 = document.querySelector(".js-ToCurrency2");
-let ToCurrency3 = document.querySelector(".js-ToCurrency3");
-let ToCurrency4 = document.querySelector(".js-ToCurrency4");
+let FromCurrency = document.querySelectorAll(".js-FromCurrency");
+let ToCurrency = document.querySelectorAll(".js-ToCurrency");
 let FromValue = document.querySelector(".js-FromValue");
 let FromCurrencySymbol = document.querySelector(".js-FromCurrencySymbol");
 let Rate = document.querySelector(".js-Rate");
 let RateCurrencySymbol = document.querySelector(".js-RateCurrencySymbol");
 let ToCurrencySymbol = document.querySelector(".js-ToCurrencySymbol");
 let ToValue = document.querySelector(".js-ToValue")
-
 let EuroRate = 4.4573;
 let GBPRate = 4.9328;
 let USDRate = 3.9741;
 let CHFRate = 4.1811;
 let PLNRate = 1.0;
 
+const CheckFromCurrency = () => {
+    for (i = 1; i <= 5; i++) {
+        if (FromCurrency[i - 1].checked === true) {
+            FromCurrencySymbol.value = FromCurrency[i - 1].value;
+            break;
+        }
+    }
+}
+form.addEventListener("input", CheckFromCurrency);
+
+
+const CheckToCurrency = () => {
+    for (i = 1; i <= 5; i++) {
+        if (ToCurrency[i - 1].checked === true) {
+            ToCurrencySymbol.value = ToCurrency[i - 1].value;
+            break;
+        }
+    }
+}
+form.addEventListener("input", CheckToCurrency);
 
 
 form.addEventListener("input", () => {
 
-    // in this section we checking from which currency we convert
-    if (FromCurrency0.checked == true) {
-        FromCurrencySymbol.value = FromCurrency0.value;
-    }
-    else if (FromCurrency1.checked == true) {
-        FromCurrencySymbol.value = FromCurrencySymbol.value = FromCurrency1.value;
-    } else if (FromCurrency2.checked == true) {
-        FromCurrencySymbol.value = FromCurrencySymbol.value = FromCurrency2.value;
-    } else if (FromCurrency3.checked == true) {
-        FromCurrencySymbol.value = FromCurrencySymbol.value = FromCurrency3.value;
-    } else if (FromCurrency4.checked == true) {
-        FromCurrencySymbol.value = FromCurrencySymbol.value = FromCurrency4.value;
-    }
-    // in this section we checking to which currency we convert
-    if (ToCurrency0.checked == true) {
-        ToCurrencySymbol.value = ToCurrency0.value;
-    }
-    else if (ToCurrency1.checked == true) {
-        ToCurrencySymbol.value = ToCurrencySymbol.value = ToCurrency1.value;
-    } else if (ToCurrency2.checked == true) {
-        ToCurrencySymbol.value = ToCurrencySymbol.value = ToCurrency2.value;
-    } else if (ToCurrency3.checked == true) {
-        ToCurrencySymbol.value = ToCurrencySymbol.value = ToCurrency3.value;
-    } else if (ToCurrency4.checked == true) {
-        ToCurrencySymbol.value = ToCurrencySymbol.value = ToCurrency4.value;
-    }
 
     //we inform about currency rate 
     if (FromCurrencySymbol.value == "EUR" && ToCurrencySymbol.value == "EUR") {
@@ -130,6 +115,7 @@ form.addEventListener("submit", (event) => {
 
 
 });
+
 
 
 
